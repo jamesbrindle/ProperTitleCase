852ded0296ec3094f2f6bc72a3cc4493789511f4
Jamie Brindle <jamie.brindle7@gmail.com>
2023-12-23 03:30:05 +0000

    Add project files.

---
+++ ProperTitleCase/Program.cs
@@ -0,0 +1,32 @@
+﻿using ProperTitleCase.Business;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ProperTitleCase
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<string> titles = File.ReadAllLines(@"D:\Desktop\Folder Structure Download - Static.csv").ToList();
+
+            List<string> additionalAbbreviations = new List<string>
+            {
+                "FAT",
+                "SAT",
+                "WAS",
+                "Psmt",
+
+            };
+
+            var processedTitles = TitleCaseConverter.ToProperTitleCase(titles, additionalAbbreviations);
+
+            foreach (var title in processedTitles)
+                Console.WriteLine(title);
+
+            Console.In.ReadLine();
+        }
+    }
+}
